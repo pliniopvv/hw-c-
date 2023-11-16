405c2a39e2b1617f8b7d9f3e494f84eca18e52fa
Plínio Pvv <gangss2@hotmail.com>
2023-11-16 12:32:03 -0300

    Tutorial: criar uma API Web com o ASP.NET Core

---
+++ README.md
@@ -0,0 +1,4 @@
+## Tutorial: criar uma API Web com o ASP.NET Core
+
+[Ir para tutorial](https://learn.microsoft.com/pt-br/aspnet/core/tutorials/first-web-api?view=aspnetcore-7.0&tabs=visual-studio)
+
---
+++ helloaspnet/helloaspnet/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace helloaspnet.Models
+{
+    public class TodoItem
+    {
+        public long id { get; set; }
+        public string? title { get; set; }
+
+        public bool IsComplete {  get; set; }
+
+    }
+}
